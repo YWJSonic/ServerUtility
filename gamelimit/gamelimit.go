3f4a1e5615cff68d2073fb82c916f63285e325ae
Sonic <sony79410@gmail.com>
2019-07-09 14:39:52 +0800

    新增 限制數值0時為無限制

--- gamelimit/gamelimit.go
+++ gamelimit/gamelimit.go
@@ -1,7 +1,11 @@
 package gamelimit
 
 // IsInTotalMoneyWinLimit if int limit return true
+// 0 is no limit
 func IsInTotalMoneyWinLimit(limit, betMoney, totalWin int64) bool {
+	if limit == 0 {
+		return true
+	}
 	if totalWin > limit {
 		return false
 	}
@@ -9,7 +13,11 @@ func IsInTotalMoneyWinLimit(limit, betMoney, totalWin int64) bool {
 }
 
 // IsInTotalBetRateWinLimit if int limit return true
+// 0 is no limit
 func IsInTotalBetRateWinLimit(limit, betMoney, totalWin int64) bool {
+	if limit == 0 {
+		return true
+	}
 	if (totalWin / betMoney) > limit {
 		return false
 	}
